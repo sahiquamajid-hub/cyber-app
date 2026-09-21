1e106b23b8cf22a57bc6df5078ec6dc72bafe2d1
sahiquamajid-hub <sahiquamajid@gmail.com>
2026-09-22 00:35:06 +0500

    Standardize vite config for tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,5 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite' // or standard plugin configuration
 
 // https://vite.dev/config/
 export default defineConfig({
