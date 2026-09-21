c97cecf529ea5f53b112f16ffdf17dbf404b83f4
sahiquamajid-hub <sahiquamajid@gmail.com>
2026-09-22 00:30:35 +0500

    Fix PostCSS build error

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite' // or standard plugin configuration
 
 // https://vite.dev/config/
 export default defineConfig({
